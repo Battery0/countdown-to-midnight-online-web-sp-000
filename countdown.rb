dcd39f94c95a02dc7b6963b40136c1602bce73fa
Ian King <kickthechair@gmail.com>
2021-05-26 13:56:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(x)
+  counter = x
+  
+  while counter != 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
